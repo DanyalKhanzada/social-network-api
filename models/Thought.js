bf76fecf569aa1bd7aa7d682d53a0c77ab9b1d9a
“Danyal <danyalkhanzada89@gmail.com>
2021-07-12 21:48:40 -0400

    thought moddelling

--- models/Thought.js
+++ models/Thought.js
@@ -29,3 +29,44 @@ const ReactionSchema = new Schema(
         }
     }
 );
+
+
+const ThoughtSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+ThoughtSchema.virtual('userId').get(function () {
+    return this.parent.__id;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
