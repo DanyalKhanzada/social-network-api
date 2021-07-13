b0f744cf552831d56dfeaad94f455d05fd0e2485
“Danyal <danyalkhanzada89@gmail.com>
2021-07-12 21:01:09 -0400

    get all thoughts

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
