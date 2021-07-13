6ffad63d2d10ef7d97583b916f8b9cacd33dae82
“Danyal <danyalkhanzada89@gmail.com>
2021-07-12 21:02:12 -0400

    add thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -13,3 +13,23 @@ const thoughtController = {
             });
     },
 
+    // add thought
+    addThought({ body }, res) {
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: body.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true, runValidators: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+};
