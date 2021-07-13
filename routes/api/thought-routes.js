68d48fbb65934449656ef257efdd7205c8acc795
“Danyal <danyalkhanzada89@gmail.com>
2021-07-13 12:35:09 -0400

    finishing touches

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router.route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+// /api/thoughts/<thoughtId>
+router.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(removeThought);
+
+// /api/thoughts/<thoughtId>/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/<thoughtId>/reactions/<reactionId>
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
